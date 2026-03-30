a4eb8d9c14f5b2c6352e8ab92fa7d247d9b79f6f
Richard Schneeman <rschneeman@salesforce.com>
2026-03-30 17:39:55 -0500

    Simplify Railtie configuration
    Remove the `statsd` and `aggregation_period` config options from
    the Rails integration since Barnes no longer uses StatsD.
    
    The Railtie now only exposes `interval` and `panels`, matching the
    simplified Barnes.start signature.

--- lib/barnes/railtie.rb
+++ lib/barnes/railtie.rb
@@ -24,20 +24,10 @@
 require 'rails/railtie'
 
 module Barnes
-  # Automatically configures barnes to run with
-  # rails 3, 4, and 5. Configuration can be changed
-  # in the application.rb. For example
-  #
-  #   module YourApp
-  #     class Application < Rails::Application
-  #     config.barnes[:interval] = 20
-  #
   class Railtie < ::Rails::Railtie
     config.barnes = {
-      interval:           DEFAULT_INTERVAL,
-      aggregation_period: DEFAULT_AGGREGATION_PERIOD,
-      statsd:             DEFAULT_STATSD,
-      panels:             DEFAULT_PANELS,
+      interval: DEFAULT_INTERVAL,
+      panels:   DEFAULT_PANELS,
     }
 
     initializer 'barnes' do |app|
