aa95df67caf10a662d17008ca125ecf61e3044e3
Schneems <richard.schneeman+foo@gmail.com>
2026-05-05 16:20:06 -0500

    Remove Process.clock_gettime guard and RubyProf fallback
    Process.clock_gettime has been available since Ruby 2.1. Now that the
    minimum Ruby version is 3.1, the respond_to? check is always true and
    the RubyProf fallback branch is dead code. The @has_cpu_time conditional
    is also no longer needed since Timepiece.cpu is always defined.

--- lib/barnes/instruments/stopwatch.rb
+++ lib/barnes/instruments/stopwatch.rb
@@ -26,7 +26,6 @@ module Barnes
     class Stopwatch
       def initialize(timepiece = Timepiece)
         @timepiece = timepiece
-        @has_cpu_time = timepiece.respond_to?(:cpu)
       end
 
       def start!(state)
@@ -36,33 +35,29 @@ module Barnes
       def instrument!(state, counters, gauges)
         last = state[:stopwatch]
         wall_elapsed = @timepiece.wall - last[:wall]
+        cpu_elapsed = @timepiece.cpu - last[:cpu]
+        idle_elapsed = wall_elapsed - cpu_elapsed
+
         counters[:'Time.wall'] = wall_elapsed
+        counters[:'Time.cpu']      = cpu_elapsed
+        counters[:'Time.idle']     = idle_elapsed
 
-        if @has_cpu_time
-          cpu_elapsed = @timepiece.cpu - last[:cpu]
-          idle_elapsed = wall_elapsed - cpu_elapsed
-
-          counters[:'Time.cpu']      = cpu_elapsed
-          counters[:'Time.idle']     = idle_elapsed
-
-          if wall_elapsed == 0
-            counters[:'Time.pct.cpu']  = 0
-            counters[:'Time.pct.idle'] = 0
-          else
-            counters[:'Time.pct.cpu']  = 100.0 * cpu_elapsed  / wall_elapsed
-            counters[:'Time.pct.idle'] = 100.0 * idle_elapsed / wall_elapsed
-          end
+        if wall_elapsed == 0
+          counters[:'Time.pct.cpu']  = 0
+          counters[:'Time.pct.idle'] = 0
+        else
+          counters[:'Time.pct.cpu']  = 100.0 * cpu_elapsed  / wall_elapsed
+          counters[:'Time.pct.idle'] = 100.0 * idle_elapsed / wall_elapsed
         end
 
         state[:stopwatch] = current
       end
 
       private def current
-        state = {
+        {
           :wall => @timepiece.wall,
+          :cpu  => @timepiece.cpu,
         }
-        state[:cpu]  = @timepiece.cpu if @has_cpu_time
-        state
       end
     end
 
@@ -71,17 +66,8 @@ module Barnes
         ::Time.now.to_f * 1000
       end
 
-      # Ruby 2.1+
-      if Process.respond_to?(:clock_gettime)
-        def self.cpu
-          Process.clock_gettime Process::CLOCK_PROCESS_CPUTIME_ID, :float_millisecond
-        end
-
-      # ruby-prof installed
-      elsif defined? RubyProf::Measure::ProcessTime
-        def self.cpu
-          RubyProf::Measure::Process.measure * 1000
-        end
+      def self.cpu
+        Process.clock_gettime Process::CLOCK_PROCESS_CPUTIME_ID, :float_millisecond
       end
     end
   end
